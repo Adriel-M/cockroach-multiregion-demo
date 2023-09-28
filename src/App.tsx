36f82c06949c93bc31b36f5365158cbb84d27d50
Adriel <20115135+Adriel-M@users.noreply.github.com>
2023-09-28 17:38:40 +0200

    I is a react dev

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as ReactDOM from 'react-dom';
+
+function render() {
+    ReactDOM.render(
+        <h2>Bonjour!</h2>,
+        document.body,
+    )
+}
+
+render();
--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -7,4 +8,5 @@ export default defineConfig({
     browserField: false,
     mainFields: ['module', 'jsnext:main', 'jsnext'],
   },
+  plugins: [react()],
 });
