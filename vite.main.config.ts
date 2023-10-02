175711d540a498fbfe4f879cad9ed583bebce43f
Adriel <20115135+Adriel-M@users.noreply.github.com>
2023-10-02 19:55:36 +0200

    Correct location of react vite plugin

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -8,5 +7,4 @@ export default defineConfig({
     browserField: false,
     mainFields: ["module", "jsnext:main", "jsnext"],
   },
-  plugins: [react()],
 });
